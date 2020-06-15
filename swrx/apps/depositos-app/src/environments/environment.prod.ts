34bbc233b0f47f44af94c278550e02278ca94ba5
Ruben Cancino <rubencancino6@gmail.com>
2020-06-15 15:31:48 -0500

    Update environment.prod para firebase PROD

--- swrx/apps/depositos-app/src/environments/environment.prod.ts
+++ swrx/apps/depositos-app/src/environments/environment.prod.ts
@@ -2,12 +2,13 @@ export const environment = {
   apiUrl: 'http://10.10.1.146:9197/callcener/api',
   production: true,
   firebase: {
-    apiKey: 'AIzaSyC4cYX9eZN4XqsX2EuYwN2GLPtb6R3r7RU',
-    authDomain: 'siipapx-436ce.firebaseapp.com',
-    databaseURL: 'https://siipapx-436ce.firebaseio.com',
-    projectId: 'siipapx-436ce',
-    storageBucket: 'siipapx-436ce.appspot.com',
-    messagingSenderId: '84446617594',
-    appId: '1:84446617594:web:e4a97a6edc5957446dce43'
+    apiKey: 'AIzaSyAWbSRZsPDwOAbMPf3zHOKkf6kNxG26W50',
+    authDomain: 'siipapx-51354.firebaseapp.com',
+    databaseURL: 'https://siipapx-51354.firebaseio.com',
+    projectId: 'siipapx-51354',
+    storageBucket: 'siipapx-51354.appspot.com',
+    messagingSenderId: '1083427339592',
+    appId: '1:1083427339592:web:a99d80ba3d9b0beaa2c77d',
+    measurementId: 'G-KMBGMQP2BM'
   }
 };
