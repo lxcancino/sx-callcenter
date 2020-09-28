a021d1abba7b0d49097ad1d831ac1d89d8966de3
Ruben Cancino <rubencancino6@gmail.com>
2020-09-28 07:32:01 -0500

    Fix

--- swrx/libs/shopping-cart/src/lib/cart-form/cart-descuentos/cart-descuentos.component.ts
+++ swrx/libs/shopping-cart/src/lib/cart-form/cart-descuentos/cart-descuentos.component.ts
@@ -22,15 +22,15 @@ export class CartDescuentosComponent implements OnInit {
   tipo: any;
   // descuentos: { descuento: number; importe: any }[];
   descuentos = [
-    { descuento: 0.0, importe: 100.0 },
-    { descuento: 10.0, importe: 1000.0 },
-    { descuento: 12.0, importe: 5000.0 },
-    { descuento: 14.0, importe: 12000.0 },
-    { descuento: 15.0, importe: 21500.0 },
-    { descuento: 16.0, importe: 46000.0 },
-    { descuento: 17.0, importe: 82000.0 },
-    { descuento: 18.0, importe: 150000.0 },
-    { descuento: 19.0, importe: 30000000.0 }
+    { descuento: 0.0, de: 0.0, hasta: 100.0 },
+    { descuento: 10.0, de: 100.0, hasta: 1000.0 },
+    { descuento: 12.0, de: 1001.0, hasta: 5000.0 },
+    { descuento: 14.0, de: 5001.0, hasta: 12000.0 },
+    { descuento: 15.0, de: 12001.0, hasta: 21500.0 },
+    { descuento: 16.0, de: 21501.0, hasta: 46000.0 },
+    { descuento: 17.0, de: 46001.0, hasta: 82000.0 },
+    { descuento: 18.0, de: 82001.0, hasta: 150000.0 },
+    { descuento: 19.0, de: 82001.0, hasta: 30000000.0 }
   ];
   displayedColumns: string[] = ['de', 'hasta', 'descuento'];
   constructor(@Inject(MAT_DIALOG_DATA) private data: any) {
